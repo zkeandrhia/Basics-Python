ccbc3876d4844e168af3ca48310b7b72649506fb
zke <85144998+zkeandrhia@users.noreply.github.com>
2021-07-21 14:22:55 +0800

    Add files via upload

---
+++ wordreplacemnet.py
@@ -0,0 +1,6 @@
+user = input('Enter a sentence: ')
+replace1 = input("Word to replace: ")
+replace2 = input('Word to replace it with: ')
+
+print('\nNew sentence\n')
+print(user.replace(replace1, replace2))
